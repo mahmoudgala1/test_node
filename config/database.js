07957674a9267024cf62dce01cfb221228456cf9
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2023-12-28 14:52:05 +0200

    :)

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const dbConnection = mysql.createConnection({
     host: process.env.MYSQL_URL,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
+    password: "",
     database: process.env.MYSQL_DATABASE_NAME,
     multipleStatements: true
 });
